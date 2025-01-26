16b4e25b0abe007524a9785398b1baa9997a8a38
Diego Reyes <reyess_diego@outlook.com>
2025-01-25 23:15:07 -0600

    Avance 11 enero

---
+++ programming-basics/recap.rb
@@ -0,0 +1,17 @@
+age = 20
+# we assign the value of 20 to the variable age
+
+age = nil
+#here we initilize when we dont yet know the value
+
+age = 25
+#We reassign the value of age
+
+age = age + 1
+# Here we increment by 1
+age +=1
+#Same as above
+
+def method_name(parameter_1, parameter_2)
+  #Does returns something here
+end
